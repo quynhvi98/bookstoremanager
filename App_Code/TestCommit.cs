fb411373c3cd1c904bed452419c98ec2a73255e3
Vi Quynh <vi.quynh@hotmail.com>
2017-10-23 10:37:19 +0700

    source code book store manager

---
+++ App_Code/TestCommit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for TestCommit
+/// </summary>
+public class TestCommit
+{
+    public TestCommit()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
